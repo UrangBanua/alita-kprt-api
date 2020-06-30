bae65d9ac9226c22f567910c3d1faa29ec8c1ac2
Afni Afdillah <58909061+afni18@users.noreply.github.com>
2020-06-30 16:14:15 +0800

    Bug fixes . .

---
+++ routes/app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var dialogflowRouter = require('./routes/dialogflow');
+var bot_apiRouter = require('./routes/bot_api');
+var bot_webhookRouter = require('./routes/bot_webhook');
+var bot_helpsRouter = require('./routes/bot_helps');
+var bot_packmealRouter = require('./routes/bot_packmeal');
+
+var app = express();
+
+// Access-Control-Allow-Origin
+app.use(function (req, res, next) {
+    //Enabling CORS 
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/dialogflow', dialogflowRouter);
+app.use('/bot_webhook', bot_webhookRouter);
+app.use('/bot_api', bot_apiRouter);
+app.use('/bot_helps', bot_helpsRouter);
+app.use('/bot_packmeal', bot_packmealRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
